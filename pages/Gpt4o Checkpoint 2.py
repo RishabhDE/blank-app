01bcbff6107334e7c40bd1fc7103f59419dc2d35
Rishabh Malik <110093478+RishabhDE@users.noreply.github.com>
2024-10-18 03:38:28 +0000

    Adding Checkpoint Models

--- pages/Gpt4o Checkpoint 2.py
+++ pages/Gpt4o Checkpoint 2.py
@@ -20,11 +20,10 @@ if prompt := st.chat_input():
     result = client.fine_tuning.jobs.list()
 
     # Retrieve the fine tuned model
-    fine_tuned_model = result.data[0].fine_tuned_model
-    print(fine_tuned_model)
+    fine_tuned_model_checkpoint = "ft:gpt-4o-mini-2024-07-18:personal::AJWkdVEz:ckpt-step-44284"
     st.session_state.messages.append({"role": "user", "content": prompt})
     st.chat_message("user").write(prompt)
-    response = client.chat.completions.create(model=fine_tuned_model, messages=st.session_state.messages)
+    response = client.chat.completions.create(model=fine_tuned_model_checkpoint, messages=st.session_state.messages)
     msg = response.choices[0].message.content
     st.session_state.messages.append({"role": "assistant", "content": msg})
     st.chat_message("assistant").write(msg)
